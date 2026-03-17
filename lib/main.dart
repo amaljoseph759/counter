78768ecd75b73c27c1d33813bc02f641331044cd
Technodysis - amalJoseph <amal.joseph@technodysis.com>
2026-03-17 15:34:54 +0530

    add new

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Conterr'),
+      home: const MyHomePage(title: 'kkatti'),
     );
   }
 }
